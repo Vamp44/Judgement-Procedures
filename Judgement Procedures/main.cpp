1027f08bc3cd4f04e6dbb2da481b37327763629f
Vamp44 <2991597771@qq.com>
2019-07-05 16:00:26 +0800

    Update main.cpp

--- Judgement Procedures/main.cpp
+++ Judgement Procedures/main.cpp
@@ -91,15 +91,17 @@ int main(int argc, const char * argv[]) {
         sort(s, s+n1, compare);
     // 写入输出
     final_s <<"                                <<<<<<<学生成绩排名表>>>>>>>"<< endl <<"+--------------------------------------------------------------------------------------+"<< endl;
-    final_s <<"|"<< left << setw(12) <<"排名"<< left << setw(26) <<"隶属学院"<< left << setw(23) <<"学号"<< left << setw(13) <<"姓名"<< left << setw(18) <<"平均成绩"<< left << setw(19) <<"联系方式"<<"|"<< endl ;
+    final_s << left << setw(1) << "|";
+    final_s << left << setw(12) <<"排名"<< left << setw(26) <<"隶属学院"<< left << setw(23) <<"学号"<< left << setw(13) <<"姓名"<< left << setw(18) <<"平均成绩"<< left << setw(19) <<"联系方式";
+    final_s << right << setw(2) <<"|"<< endl;
     for(int i=0; i<n1; i++)
     {
-        final_s << "|";
+        final_s << left << setw(1) << "|";
         if(i<9)
             final_s <<"第"<< i+1 << left << setw(8) <<"名"<< left << setw(29) << s[i].school << left << setw(20) << s[i].stu_num << left << setw(15) << s[i].name_s << left << setw(11) << s[i].ave << left << setw(14) << s[i].tel_num;
         else
             final_s <<"第"<< i+1 << left << setw(7) <<"名"<< left << setw(29) << s[i].school << left << setw(20) << s[i].stu_num << left << setw(15) << s[i].name_s << left << setw(11) << s[i].ave << left << setw(14) << s[i].tel_num;
-        final_s <<"|"<< endl;
+        final_s << right << setw(2) <<"|"<< endl;
     }
     final_s <<"+--------------------------------------------------------------------------------------+"<< endl <<"                                <<<<<<<<<<<*结束*>>>>>>>>>>>"<< endl;
     final_s.close();
