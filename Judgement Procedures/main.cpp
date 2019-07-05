df624f8e6a789eb445d720ba6a877729c82345bf
Vamp44 <2991597771@qq.com>
2019-07-05 17:50:40 +0800

    Update main.cpp

--- Judgement Procedures/main.cpp
+++ Judgement Procedures/main.cpp
@@ -15,7 +15,7 @@ using namespace std;
 
 ifstream stu("/Users/nianqingren/Desktop/accurac/JudgementProcedures/student.txt");
 ifstream judge("/Users/nianqingren/Desktop/accurac/JudgementProcedures/judge.txt");
-ofstream final_s("/Users/nianqingren/Desktop/accurac/JudgementProcedures/final.txt");
+ofstream final_s("/Users/nianqingren/Desktop/accurac/JudgementProcedures/final.csv");
 
 const int maxn = 50;
 struct student
@@ -90,20 +90,20 @@ int main(int argc, const char * argv[]) {
     }
         sort(s, s+n1, compare);
     // 写入输出
-    final_s <<"                                <<<<<<<学生成绩排名表>>>>>>>"<< endl <<"+---------------------------------------------------------------------------------------+"<< endl;
-    final_s << left << setw(1) << "|";
-    final_s << left << setw(12) <<"排名"<< left << setw(26) <<"隶属学院"<< left << setw(23) <<"学号"<< left << setw(13) <<"姓名"<< left << setw(18) <<"平均成绩"<< left << setw(19) <<"联系方式";
-    final_s << right << setw(2) <<"|"<< endl;
+    /*final_s <<"                                <<<<<<<学生成绩排名表>>>>>>>"<< endl <<"+---------------------------------------------------------------------------------------+"<< endl;
+    final_s << left << setw(1) << "|";*/
+    final_s << left << setw(12) <<"排名"<< left << setw(26) <<"隶属学院"<< left << setw(23) <<"学号"<< left << setw(13) <<"姓名"<< left << setw(18) <<"平均成绩"<< left << setw(19) <<"联系方式"<<endl;
+   // final_s << right << setw(2) <<"|"<< endl;
     for(int i=0; i<n1; i++)
     {
-        final_s << left << setw(1) << "|";
+       // final_s << left << setw(1) << "|";
         if(i<9)
-            final_s <<"第"<< i+1 << left << setw(8) <<"名"<< left << setw(29) << s[i].school << left << setw(20) << s[i].stu_num << left << setw(15) << s[i].name_s << left << setw(11) << s[i].ave << left << setw(14) << s[i].tel_num;
+            final_s <<"第"<< i+1 << left << setw(8) <<"名"<< left << setw(29) << s[i].school << left << setw(20) << s[i].stu_num << left << setw(15) << s[i].name_s << left << setw(11) << s[i].ave << left << setw(14) << s[i].tel_num<<endl;
         else
-            final_s <<"第"<< i+1 << left << setw(7) <<"名"<< left << setw(29) << s[i].school << left << setw(20) << s[i].stu_num << left << setw(15) << s[i].name_s << left << setw(11) << s[i].ave << left << setw(14) << s[i].tel_num;
-        final_s << right << setw(2) <<"|"<< endl;
+            final_s <<"第"<< i+1 << left << setw(7) <<"名"<< left << setw(29) << s[i].school << left << setw(20) << s[i].stu_num << left << setw(15) << s[i].name_s << left << setw(11) << s[i].ave << left << setw(14) << s[i].tel_num<<endl;
+       // final_s << right << setw(2) <<"|"<< endl;
     }
-    final_s <<"+---------------------------------------------------------------------------------------+"<< endl <<"                                <<<<<<<<<<<*结束*>>>>>>>>>>>"<< endl;
+   /* final_s <<"+---------------------------------------------------------------------------------------+"<< endl <<"                                <<<<<<<<<<<*结束*>>>>>>>>>>>"<< endl;*/
     final_s.close();
     return 0;
 }
