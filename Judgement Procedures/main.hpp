20af712094ad7770b57af9261d3a8ecc227118c5
Vamp44 <2991597771@qq.com>
2019-08-28 11:52:26 +0800

    Create main.hpp

---
+++ Judgement Procedures/main.hpp
@@ -0,0 +1,14 @@
+//
+//  main.hpp
+//  Judgement Procedures
+//
+//  Created by s2018 on 2019/8/27.
+//  Copyright © 2019 s2018. All rights reserved.
+//
+
+#ifndef main_hpp
+#define main_hpp
+
+#include <stdio.h>
+
+#endif /* main_hpp */
